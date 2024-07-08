cf750c27936e706f804508d80716eb4ab279d259
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2024-07-08 14:08:43 +0800

    next commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import SectionTitle from "./SectionTitle";
 const Projects = () => {
   return (
     <section className="py-20 align-element" id="projects">
-      <SectionTitle text="web creations" />
+      <SectionTitle text="projects" />
       <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {
           return <ProjectCard key={project.id} {...project} />;
